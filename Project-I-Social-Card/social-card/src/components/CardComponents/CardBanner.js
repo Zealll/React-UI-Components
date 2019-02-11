89debc4c024fe50a0cd969d75ffb5f44ecaef551
unknown <eriznis@gmail.com>
2019-02-11 16:48:33 -0500

    done with card components, MVP finished

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+import CardContent from './CardContent';
+
+const CardBanner = () => {
+    return (
+        <div className="cardDiv">
+            <img className="cardBanner" alt="Banner od React Logo" src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"></img>
+            <CardContent />
+        </div>
+        
+    )
+}
+
+export default CardBanner
