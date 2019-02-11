587b3de5a480b407d963a5ff0b21dcd0dadbf9f3
unknown <eriznis@gmail.com>
2019-02-11 15:34:57 -0500

    Header Components done

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+
+const HeaderTitle = () => {
+    return (
+        <div className="headerContent">
+        <h1>Lambda School</h1>
+        <HeaderContent />
+        </div>
+    )
+}
+
+export default HeaderTitle
