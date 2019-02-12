b6ea7061e1715b7a99e2a11917bcbaf3f7cfaf61
unknown <eriznis@gmail.com>
2019-02-11 19:52:29 -0500

    Date stretch goal. Did it with help

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -23,6 +23,7 @@ const App = () => {
       </p> */}
     </div>
   );
+  
 };
 
 export default App;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Header.css';
 import HeaderContent from './HeaderContent';
+import moment from 'moment'
 
 const HeaderTitle = () => {
     return (
         <div className="headerContent">
-        <h1>Lambda School</h1>
+        <h1>Lambda School <span className="atSpan">@LambdaSchool {moment().format('D MMM')}</span> </h1>
         <HeaderContent />
         </div>
     )
