c41a37e99c0a551560ca8783f2599de42e651618
unknown <eriznis@gmail.com>
2019-02-12 16:22:09 -0500

    done with the first set of numbers

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
-import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
     <div className="appContainer">
       <CalculatorDisplay display="calculatorDisplay" text="0"/>
+      <div className="flex">
+        <ActionButton actionButton="clear-zero" text="Clear"/>
+        <NumberButton numberButton="red" text="/"/>
+      </div>
+      <div className="flex">
+        <NumberButton numberButton="number" text="7"/>
+        <NumberButton numberButton="number" text="8"/>
+        <NumberButton numberButton="number" text="9"/>
+        <NumberButton numberButton="red" text="X"/>
+      </div>
     </div>
+    
+    
     
 
 
